61946e10bab3eeb705bb90a01a0e0d31237611f8
Ivan Stefanenko <39599113+C1oudConnected@users.noreply.github.com>
2018-05-30 03:25:36 +0300

    Add files via upload

---
+++ absolute_value.c
@@ -0,0 +1,18 @@
+int absolute_value(int v) {
+	int lastBit = 1;
+    while (lastBit<<1)
+		lastBit <<= 1;
+    return v * ((v & lastBit)/lastBit * (-2) + 1);	
+}
+
+// If there is sizeof();
+
+int absolute_value_C(int v) {
+    return v * (v & 1<<(sizeof(v*8)-1))/(1<<(sizeof(v*8)-1)) * (-2) + 1);	
+}
+
+
+
+
+
+
