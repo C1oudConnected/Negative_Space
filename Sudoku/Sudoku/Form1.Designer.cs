87e51b5550388409273622f01e2fe2a652a2ee9a
Ivan Stefanenko <39599113+C1oudConnected@users.noreply.github.com>
2018-05-30 03:51:30 +0300

    Add files via upload

---
+++ Sudoku/Sudoku/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace Sudoku
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.gameField = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.gameField)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gameField
+            // 
+            this.gameField.AllowUserToAddRows = false;
+            this.gameField.AllowUserToDeleteRows = false;
+            this.gameField.AllowUserToResizeColumns = false;
+            this.gameField.AllowUserToResizeRows = false;
+            this.gameField.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gameField.ColumnHeadersVisible = false;
+            this.gameField.Location = new System.Drawing.Point(13, 13);
+            this.gameField.Margin = new System.Windows.Forms.Padding(4);
+            this.gameField.Name = "gameField";
+            this.gameField.RowHeadersVisible = false;
+            this.gameField.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.gameField.Size = new System.Drawing.Size(612, 567);
+            this.gameField.TabIndex = 0;
+            this.gameField.CellEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.gameField_CellEnter);
+            this.gameField.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.gameField_CellValueChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.button1.Location = new System.Drawing.Point(633, 17);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Level 1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.button2.Location = new System.Drawing.Point(633, 53);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 28);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Level 2";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.button3.Location = new System.Drawing.Point(633, 89);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 28);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Level 3";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Mistral", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Lime;
+            this.label1.Location = new System.Drawing.Point(14, 586);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 57);
+            this.label1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Mistral", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(13, 586);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 57);
+            this.label2.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.ClientSize = new System.Drawing.Size(739, 655);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.gameField);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Sudoku!";
+            ((System.ComponentModel.ISupportInitialize)(this.gameField)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView gameField;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Sudoku/Sudoku/Form1.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sudoku
+{
+    public partial class Form1 : Form
+    {
+        int count; // Кол-во не_пустых ячеек.
+        object prev; // Значение ячейки до изменения.
+        List<Error> errors = new List<Error>(); // Коллекция ошибок в виде пары повторяющихся элементов.
+        bool isReady; // Индикатор завершенной автогенерации поля.
+
+        public Form1() 
+        {
+            InitializeComponent();
+            gameField.RowCount = 9;
+            gameField.ColumnCount = 9;
+            gameField.ScrollBars = ScrollBars.None;
+            gameField.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            gameField.DefaultCellStyle.Font = new Font(gameField.DefaultCellStyle.Font.FontFamily, 25);
+            for (int i = 0; i < 9; i++)
+            {
+                gameField.Columns[i].Width = (gameField.Size.Width) / 9;
+                gameField.Rows[i].Height = (gameField.Size.Height) / 9;
+            }
+            generateField(1);
+        } // Создание поля и запуск его генерации.
+
+        class Error
+        {
+            public int[] code;
+            public Error(int[] _code)
+            {
+                code = _code;
+            }
+        } // Класс, представляющий из себя пару "координат" - ячеек, некорректно имеющих равные значения.
+
+        void generateField(int lvl)
+        {
+            isReady = false;
+            errors.Clear();
+            count = 0;
+            label1.Text = "";            
+            for (int i = 0; i < 9; i++)
+                for (int j = 0; j < 9; j++)
+                {
+                    gameField.Rows[i].Cells[j].Value = null;
+                    gameField.Rows[i].Cells[j].ReadOnly = false;
+                    gameField.Rows[i].Cells[j].Style.ForeColor = Color.Black;
+                    if ((3 * (i / 3) + j / 3) % 2 == 0) gameField.Rows[i].Cells[j].Style.BackColor = Color.Aqua;
+                    else gameField.Rows[i].Cells[j].Style.BackColor = Color.Aquamarine;
+                }
+            for (int i = 0; i < 50 - 12 * lvl; i++)
+            {
+                int a = generateCell();
+                gameField.Rows[a / 9].Cells[a % 9].Value = (((a / 9) * 3 + (a / 9) / 3 + (a % 9)) % 9 + 1); count++;
+            }
+            Random r = new Random();
+            for (int i = 0; i < 1000; i++)
+            {
+                shuffleField(r);
+            }
+            for (int i = 0; i < 81; i++)
+            {
+                if (gameField.Rows[i / 9].Cells[i % 9].Value != null)
+                {
+                    gameField.Rows[i / 9].Cells[i % 9].ReadOnly = true;
+                    gameField.Rows[i / 9].Cells[i % 9].Style.ForeColor = Color.DarkGreen;
+                }
+            }
+            isReady = true;
+        } // Генерация поля. Часично создается изначальная сетка по формуле, попутно увеличивая счетчик, кол-во созданных эелементов зависит от уровня сложности. Потом сетка (псевдо)случайно резмешивается.
+
+        void shuffleField(Random r)
+        {
+            int i;
+            switch (r.Next() % 4)
+            {
+                case 0:
+                    i = shuffleLine(r);
+                    swapLines(i / 9, i % 9);
+                    break;
+                case 1:
+                    i = shuffleLine(r);
+                    swapColumns(i / 9, i % 9);
+                    break;
+                case 2:
+                    i = shuffleRow(r);
+                    swapLines(3 * (i / 3), 3 * (i % 3));
+                    swapLines(3 * (i / 3) + 1, 3 * (i % 3) + 1);
+                    swapLines(3 * (i / 3) + 2, 3 * (i % 3) + 2);
+                    break;
+                case 3:
+                    i = shuffleRow(r);
+                    swapColumns(3 * (i / 3), 3 * (i % 3));
+                    swapColumns(3 * (i / 3) + 1, 3 * (i % 3) + 1);
+                    swapColumns(3 * (i / 3) + 2, 3 * (i % 3) + 2);
+                    break;
+            }
+
+        } // Перетасовка поля.
+
+        int shuffleLine(Random r)
+        {
+            int b = r.Next() % 9;
+            int i;
+            do
+            {
+                i = r.Next() % 3 + 3 * (b / 3);
+            }
+            while (i == b);
+            return i * 9 + b;
+        } // Поиск доступных к обмену линий (рядов или колонн).
+
+        int shuffleRow(Random r)
+        {
+            int b = r.Next() % 3;
+            int i;
+            do
+            {
+                i = r.Next() % 3;
+            }
+            while (i == b);
+            return 3 * i + b;
+        } // Поиск доступных к обмену площадей (триад рядов или колонн).
+
+        void swapLines(int a, int b)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                object buffer = gameField.Rows[a].Cells[i].Value;
+                gameField.Rows[a].Cells[i].Value = gameField.Rows[b].Cells[i].Value;
+                gameField.Rows[b].Cells[i].Value = buffer;
+            }
+        } // Обмен между двумя рядами.
+
+        void swapColumns(int a, int b)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                object buffer = gameField.Rows[i].Cells[a].Value;
+                gameField.Rows[i].Cells[a].Value = gameField.Rows[i].Cells[b].Value;
+                gameField.Rows[i].Cells[b].Value = buffer;
+            }
+        } // Обмен между двумя колоннами.
+
+        int generateCell()
+        {
+            Random rnd = new Random();
+            int i;
+            do
+            {
+                i = rnd.Next() % 81;
+            }
+            while (gameField.Rows[i / 9].Cells[i % 9].Value != null);
+            return i;
+        } // Получение случайной незанятой позиции на поле.
+
+
+        //=========== Уровни сложности ============
+        private void button1_Click(object sender, EventArgs e)
+        {
+            generateField(1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            generateField(2);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            generateField(3);
+        }
+
+
+        //=========== Обработка хода игрока =======
+        private void gameField_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            if (isReady)
+            {
+                if (prev == null) count++;
+                if (gameField.Rows[e.RowIndex].Cells[e.ColumnIndex].Value != null && Convert.ToInt32(gameField.Rows[e.RowIndex].Cells[e.ColumnIndex].Value) <= 9 && Convert.ToInt32(gameField.Rows[e.RowIndex].Cells[e.ColumnIndex].Value) > 0)
+                     checkCell(e.RowIndex, e.ColumnIndex);
+                else gameField.Rows[e.RowIndex].Cells[e.ColumnIndex].Value = prev;
+                if (errors.Count == 0 && count == 81)
+                    congrats();
+            }
+        } // Обработчик события смены значения ячейка. Вызов проверяющей и завершающей функции.
+
+        void checkCell(int r, int c)
+        {
+            int startPt = 9 * 3 * (r / 3) + 3 * (c / 3);
+            gameField.Rows[r].Cells[c].Style.BackColor = ((3 * (r / 3) + c / 3) % 2 == 0) ? Color.Aqua : Color.Aquamarine;
+            for (int i = 0; i < 9; i++)
+            {
+                if (i != c && gameField.Rows[r].Cells[i].Value != null)
+                    check(r, c, r, i);
+                if (i != r && gameField.Rows[i].Cells[c].Value != null)
+                    check(r, c, i, c);
+                int coordinateCode = (startPt + 9 * (i / 3) + i % 3);
+
+                if (coordinateCode != 9 * r + c && gameField.Rows[coordinateCode / 9].Cells[coordinateCode % 9].Value != null)
+                    check(r, c, coordinateCode / 9, coordinateCode % 9);
+            }
+        } // Сравнивает ячейку с ее соседями по ряду, колонне и площади.
+
+        void check(int r1, int c1, int r2, int c2)
+        {
+            int code1 = r1 * 9 + c1, code2 = r2 * 9 + c2;
+            bool idx;
+            if (Convert.ToInt32(gameField.Rows[r1].Cells[c1].Value) == Convert.ToInt32(gameField.Rows[r2].Cells[c2].Value))
+            {
+                gameField.Rows[r1].Cells[c1].Style.BackColor = Color.Red;
+                gameField.Rows[r2].Cells[c2].Style.BackColor = Color.Red;
+                errors.Add(new Error(new int[2] { code1, code2 }));
+            }
+            else
+            {
+                gameField.Rows[r2].Cells[c2].Style.BackColor = ((3 * (r2 / 3) + c2 / 3) % 2 == 0) ? Color.Aqua : Color.Aquamarine;
+                for (int i = 0; i < errors.Count; i++)
+                    if ((errors[i].code[0] == code2) || (errors[i].code[1] == code2))
+                    {
+                        idx = (errors[i].code[0] == code2) ? false : true;
+                        if (errors[i].code[Convert.ToInt32(!idx)] == code1)
+                            errors.RemoveAt(i);
+                        else gameField.Rows[r2].Cells[c2].Style.BackColor = Color.Red;
+                    }
+            }
+        } // Сравнивает две ячейки и отмечает ошибки.
+
+        void congrats()
+        { label1.Text = "Congratulations!"; } // Завершение игры, победа игрока, вызывается, когда не остается пустых ячеек поля и при этом отсутствуют ошибки.
+
+        private void gameField_CellEnter(object sender, DataGridViewCellEventArgs e)
+        { prev = gameField.Rows[e.RowIndex].Cells[e.ColumnIndex].Value; } // Получвет значение ячейки перед редактирванием. Важно для того, чтобы в случае недопустимого редактирования обратить изменения, а также чтобы увеличивать счетчик отмеченных ячеек.
+    }
+}
