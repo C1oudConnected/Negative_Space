56eadc6bf07264225f8f449124203a901b2e9cda
Ivan Stefanenko <39599113+C1oudConnected@users.noreply.github.com>
2018-05-30 03:41:51 +0300

    Add files via upload

---
+++ Spiral/Spiral/Form1.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace Spiral
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.RB = new System.Windows.Forms.CheckBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.R = new System.Windows.Forms.Label();
+            this.G = new System.Windows.Forms.Label();
+            this.B = new System.Windows.Forms.Label();
+            this.drawButton = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(444, 34);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(29, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "1";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Location = new System.Drawing.Point(444, 61);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(29, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "1";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(404, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Set N";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Location = new System.Drawing.Point(404, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Set R";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox3.Location = new System.Drawing.Point(444, 88);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(29, 20);
+            this.textBox3.TabIndex = 8;
+            this.textBox3.Text = "0";
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Location = new System.Drawing.Point(391, 91);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Angle PI";
+            // 
+            // RB
+            // 
+            this.RB.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.RB.AutoSize = true;
+            this.RB.BackColor = System.Drawing.Color.Transparent;
+            this.RB.Location = new System.Drawing.Point(407, 203);
+            this.RB.Name = "RB";
+            this.RB.Size = new System.Drawing.Size(68, 17);
+            this.RB.TabIndex = 13;
+            this.RB.Text = "Rainbow";
+            this.RB.UseVisualStyleBackColor = false;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox4.Location = new System.Drawing.Point(444, 120);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(29, 20);
+            this.textBox4.TabIndex = 14;
+            this.textBox4.Text = "0";
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox5.Location = new System.Drawing.Point(444, 146);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(29, 20);
+            this.textBox5.TabIndex = 15;
+            this.textBox5.Text = "0";
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox6.Location = new System.Drawing.Point(444, 173);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(29, 20);
+            this.textBox6.TabIndex = 16;
+            this.textBox6.Text = "0";
+            this.textBox6.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            // 
+            // R
+            // 
+            this.R.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.R.AutoSize = true;
+            this.R.BackColor = System.Drawing.Color.Transparent;
+            this.R.Location = new System.Drawing.Point(424, 123);
+            this.R.Name = "R";
+            this.R.Size = new System.Drawing.Size(15, 13);
+            this.R.TabIndex = 17;
+            this.R.Text = "R";
+            // 
+            // G
+            // 
+            this.G.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.G.AutoSize = true;
+            this.G.BackColor = System.Drawing.Color.Transparent;
+            this.G.Location = new System.Drawing.Point(424, 149);
+            this.G.Name = "G";
+            this.G.Size = new System.Drawing.Size(15, 13);
+            this.G.TabIndex = 18;
+            this.G.Text = "G";
+            // 
+            // B
+            // 
+            this.B.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.B.AutoSize = true;
+            this.B.BackColor = System.Drawing.Color.Transparent;
+            this.B.Location = new System.Drawing.Point(424, 176);
+            this.B.Name = "B";
+            this.B.Size = new System.Drawing.Size(14, 13);
+            this.B.TabIndex = 19;
+            this.B.Text = "B";
+            // 
+            // drawButton
+            // 
+            this.drawButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.drawButton.Location = new System.Drawing.Point(400, 226);
+            this.drawButton.Name = "drawButton";
+            this.drawButton.Size = new System.Drawing.Size(75, 23);
+            this.drawButton.TabIndex = 20;
+            this.drawButton.Text = "Draw";
+            this.drawButton.UseVisualStyleBackColor = true;
+            this.drawButton.Click += new System.EventHandler(this.drawButton_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(13, 11);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(78, 17);
+            this.checkBox1.TabIndex = 21;
+            this.checkBox1.Text = "Show grids";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.numericUpDown1.Location = new System.Drawing.Point(427, 8);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(45, 20);
+            this.numericUpDown1.TabIndex = 22;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(374, 10);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 23;
+            this.label3.Text = "Pen size";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(484, 511);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.drawButton);
+            this.Controls.Add(this.B);
+            this.Controls.Add(this.G);
+            this.Controls.Add(this.R);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.RB);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Spiral v2.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResizeEnd += new System.EventHandler(this.Form1_ResizeEnd_1);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.CheckBox RB;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label R;
+        private System.Windows.Forms.Label G;
+        private System.Windows.Forms.Label B;
+        private System.Windows.Forms.Button drawButton;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Spiral/Spiral/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Spiral {
+    public partial class Form1 : Form {
+
+        static bool isVis = false;
+        static bool spIsVis = false;
+
+        PictureBox spiral = new PictureBox(), grids = new PictureBox();
+        Graphics g, g2;
+        Bitmap btm, btm1;
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            this.Controls.Add(spiral);
+            this.Controls.Add(grids);
+            spiral.Controls.Add(grids);
+            spiral.Size = this.ClientSize;
+            grids.Size = this.ClientSize;
+
+            btm = new Bitmap(1000, 1000);
+            g = Graphics.FromImage(btm);
+            spiral.BackgroundImage = btm;
+            spiral.BackColor = Color.Transparent;
+            spiral.BackgroundImageLayout = ImageLayout.Zoom;
+
+            btm1 = new Bitmap(1000, 1000);
+            g2 = Graphics.FromImage(btm1);
+            grids.BackgroundImage = btm1;
+            grids.BackColor = Color.Transparent;
+            grids.BackgroundImageLayout = ImageLayout.Zoom;
+
+            drawButton_Click(sender, e);
+        }
+
+        private void lines()
+        {
+            g2.Clear(Color.Transparent);
+            Pen pen2 = new Pen(Color.Black, 3);
+
+            g2.DrawLine(pen2, 499, 0, 499, 1000);
+            g2.DrawLine(pen2, 0, 499, 1000, 499);
+            for (int i = 19; i > 0; i--)
+            {
+                g2.DrawLine(pen2, 505, (i * 50), 493, (i * 50));
+                g2.DrawLine(pen2, (i * 50), 505, (i * 50), 493);
+            }
+            pen2.Dispose();
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            if (!isVis) grids.Show();
+            else grids.Hide();
+            isVis = !isVis;
+        }
+
+         void get255(ref double a, ref double b, ref double c, double n) {
+            c += n;
+            b += c / 255;
+            a += b / 255;
+            c = c % 256;
+            b = b % 256;
+            a = a % 256;
+                
+        }
+
+        private void spiralDraw() { 
+            g.Clear(Color.Transparent);
+            double c1 = 0, c2 = 0, c3 = 0;
+
+            Pen pen = new Pen(Color.FromArgb(255, Int32.Parse(textBox4.Text), Int32.Parse(textBox5.Text), Int32.Parse(textBox6.Text)), (float)numericUpDown1.Value);
+            double angle = double.Parse(textBox3.Text) * Math.PI/2;
+            double a = angle;
+            double r = 1;
+            int n = Int32.Parse(textBox1.Text);
+            double rad = Int32.Parse(textBox2.Text) * 50;
+            double f = 1.0 * rad / n;
+            double r2 = r;
+            double a2 = a;
+            int mult255 = 255 * 255 * 255;
+            while (a <= n * Math.PI * 2 + angle)
+            {
+
+                if (RB.Checked)
+                {
+                    pen.Color = Color.FromArgb(255, (int)c1, (int)c2, (int)c3);
+                    get255(ref c1, ref c2, ref c3, (1.0 / r * mult255) / n * Math.PI * 2);
+                }
+                g.DrawEllipse(pen, (float)(499 + (r * Math.Cos(a + angle))), (float)(499 + -r * (Math.Sin(a + angle))), 1, 1);
+                a += 1.0 / r;
+                r += 1.0 / ((2 * Math.PI * (r)) / (f));
+            }
+            spiral.BackgroundImage = btm;
+            pen.Dispose();
+        }
+
+        private void Form1_ResizeEnd_1(object sender, EventArgs e) {
+            Control control = (Control)sender;
+            spiral.Size = this.ClientSize;
+            grids.Size = this.ClientSize;
+            grids.Refresh();
+            spiral.Refresh();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e) {
+            int g;
+            if (!Int32.TryParse(textBox1.Text, out g)) {
+                textBox1.Text = "1";
+            };
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e) {
+            double g;
+            if (!Double.TryParse(textBox2.Text, out g)) {
+                textBox2.Text = "1";
+            };
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e) {
+            double g;
+            if (!Double.TryParse(textBox3.Text, out g)) {
+                textBox3.Text = "0";
+            };
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+            int g;
+            if (Int32.Parse(textBox6.Text) > 255 && Int32.Parse(textBox6.Text) < 0 && !Int32.TryParse(textBox6.Text, out g))
+            {
+                textBox3.Text = "0";
+            };
+        }
+
+
+        private void drawButton_Click(object sender, EventArgs e)
+        {
+            lines();
+            spiralDraw();
+            grids.Refresh();
+            spiral.Refresh();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true) grids.Show();
+            else grids.Hide();
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            if (numericUpDown1.Value < 0) numericUpDown1.Value = 1;
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+            int g;
+            if (Int32.Parse(textBox6.Text) > 255 && Int32.Parse(textBox6.Text) < 0 && !Int32.TryParse(textBox5.Text, out g))
+            {
+                textBox3.Text = "0";
+            };
+        }
+
+        
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            int g;
+            if (Int32.Parse(textBox6.Text) > 255 && Int32.Parse(textBox6.Text) < 0 && !Int32.TryParse(textBox4.Text, out g))
+            {
+                textBox3.Text = "0";
+            };
+        }
+    }
+}
