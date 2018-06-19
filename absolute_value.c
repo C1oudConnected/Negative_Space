e958e0104080a07488f95484abf5a3a0d7095cfb
Ivan Stefanenko <39599113+C1oudConnected@users.noreply.github.com>
2018-06-20 01:02:07 +0300

    Update absolute_value.c

--- absolute_value.c
+++ absolute_value.c
@@ -3,7 +3,7 @@ T - integer type of unknown size
 
 T absolute_value_C(T v) {
 	// with Sign Extension
-	return v ^ (v>>(sizeof(v)<<3)) + v>>(sizeof(v)<<3);
+	return v ^ (v>>(sizeof(v)<<3)) + (v>>(sizeof(v)<<3)) & 1;
 	
 	
 	// without Sign Extension and sizeof() (unfinished)
